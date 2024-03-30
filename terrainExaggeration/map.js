5097bbb21c5d3734dae24e49c116c196c661b674
Qinluyi <2378716408@qq.com>
2024-03-30 15:40:42 +0800

    2024.3.30

---
+++ terrainExaggeration/map.js
@@ -0,0 +1,57 @@
+// import * as mars3d from "mars3d"
+
+var map // mars3d.Map三维地图对象
+
+// 需要覆盖config.json中地图属性参数（当前示例框架中自动处理合并）
+var mapOptions = {
+  scene: {
+    center: { lat: 32.086616, lng: 118.731447, alt: 97704, heading: 244, pitch: -22 }
+  },
+  basemaps: [
+    {
+      name: "ArcGIS影像",
+      icon: "/img/basemaps/esriWorldImagery.png",
+      type: "xyz",
+      url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      show: true,
+      enablePickFeatures: false
+    }
+  ]
+}
+
+/**
+ * 初始化地图业务，生命周期钩子函数（必须）
+ * 框架在地图初始化完成后自动调用该函数
+ * @param {mars3d.Map} mapInstance 地图对象
+ * @returns {void} 无
+ */
+function onMounted(mapInstance) {
+  map = mapInstance // 记录map
+
+  map.scene.globe.terrainExaggeration = 10 // 修改地形夸张程度
+}
+
+/**
+ * 释放当前地图业务的生命周期函数
+ * @returns {void} 无
+ */
+function onUnmounted() {
+  map = null
+}
+
+/**
+ * 地形夸张程度改变
+ * @param {number} val 默认值1.0
+ * @returns {void}
+ */
+function changeTerrain(val) {
+  map.scene.globe.terrainExaggeration = val
+}
+
+function mapFlyToPoint() {
+  map.flyToPoint([113.939351, 36.068144, 350.9], { radius: 50000 })
+}
+
+function mapSetCameraView() {
+  map.setCameraView({ lat: 26.8764, lng: 91.148781, alt: 223798, heading: 0, pitch: -45 })
+}
